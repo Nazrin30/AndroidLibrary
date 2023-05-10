a399d6ffdfa6fed6b06159b7af18bbe76dffd138
Nazrin30 <nezrin.mamedova@yahoo.com>
2023-05-10 14:38:51 +0400

    commit

--- app/src/main/java/com/example/androidlibrary/MainActivity.kt
+++ app/src/main/java/com/example/androidlibrary/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.androidlibrary
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.sdk_library.showToast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
     }
 }
